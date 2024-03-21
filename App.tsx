0cdebb2468a07613deb91c5a5963156bddbb3743
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2024-03-21 00:52:26 -0300

    update app header

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ export default function App() {
 
 	return (
 		<>
-			<AppHeader/>
+			<AppHeader appName={"Sobrevivencialismo"} />
 			<RootStack />
 		</>
 	);
--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,22 +1,30 @@
+import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 
 
-export default function AppHeader({ props }: { props?: any }) {
+export default function AppHeader({ appName, ...props }: { appName: string, props?: any }) {
     return (
-        <View className={"flex-row space-x-10 justify-between border-2 border-sky-500 rounded-lg bg-gray-300 mt-10 mx-4"}>
-            <View className={"flex-col items-start justify-center m-2 bg-green-300"}>
+        <View className={"flex-row space-x-10 justify-between border-2 border-gray-500 rounded-lg bg-gray-300 mt-10 mx-4"}>
+            <View className={"flex-col justify-center m-2"}>
+                {/* <View className={"flex-col items-start justify-center m-2 bg-green-300"}></View> */}
                 <Text
                     className={"flex-row text-lg font-bold  leading-6 text-center"}>
                     {/* {props?.children} */}
-                    Sobrevivencialismo
+                    {appName}
                 </Text>
             </View>
-
-            <View className={"flex-col items-end m-2 border-black-500 rounded-full bg-red-700 w-10 h-10"}>
-                <FontAwesome size={28} style={{ }}/>
+            
+            {/* <View className={"flex-col items-end m-2 border-black-500 rounded-full bg-red-700 w-10 h-10"}> */}
+            <View className={"flex-col items-end m-2"}>
+                <Ionicons name="person-circle" size={30} style={{}} />
+                {/* <FontAwesome size={28} style={{}} /> */}
+                {/* <Image
+                    style={{ width: 30, height: 30, }}
+                    source={{ uri: 'https://reactnative.dev/img/tiny_logo.png', }}
+                /> */}
             </View>
         </View>
     );
