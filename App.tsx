31959cb83249395e2d1f2441825d2db6742a962c
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2024-03-22 21:27:51 -0300

    att component progress line

--- App.tsx
+++ App.tsx
@@ -7,11 +7,13 @@ import "react-native-gesture-handler";
 
 import RootStack from "./src/navigation";
 import AppHeader from '~/components/AppHeader';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
 	return (
 		<>
+			<StatusBar backgroundColor={"#38A69D"} barStyle={"light-content"} />
 			<AppHeader appName={"Sobrevivencialismo"} />
 			<RootStack />
 		</>
--- src/components/ProgressLine/index.tsx
+++ src/components/ProgressLine/index.tsx
@@ -1,29 +1,72 @@
 import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import React from "react";
-import { Pressable, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Pressable, ProgressBarAndroidComponent, Text, View } from "react-native";
+// import { ProgressBar, Colors } from 'react-native-paper';
 
 
 
-export default function ProgressLine({ link, name, atual, ideal }: { link: string, name: string, atual: string, ideal: string, props?: any }) {
+export default function ProgressLine({ link, name, atual, ideal }: { link: string, name: string, atual: number, ideal: number, props?: any }) {
+
+    // const [qtdPercent, setQtdpercent] = useState(atual / ideal)
+    const [qtdPercent, setQtdpercent] = useState(4 / 5)
+    const [ fraction, setFraction] = useState("")
+
+    // function toFraction(qtdPercent: number) {
+    //     //console.log(qtdPercent * 100 / 20)
+    //     var a = "" + (qtdPercent * 100 / 20) + "/5"
+    //      console.log(a)
+    //     return a
+    // }
+    
+    // useEffect(() => {
+    //     setFraction(toFraction(qtdPercent))
+    //     console.log(fraction)
+    // }, [qtdPercent])
+    function toFraction(qtdPercent: number) {
+        //console.log(qtdPercent * 100 / 20)
+        var a = "" + (qtdPercent * 100 / 20) + "/5"
+        console.log(a)
+        setFraction(a)
+    }
+
+    useEffect(() => {
+        toFraction(qtdPercent)
+        console.log(fraction)
+    }, [])
+
+
     return (
-        <View className={"flex flex-col rounded-lg mx-1 w-full"}>
-            <View className={"mx-5 flex-row items-center border-2 border-gray-400 rounded-lg bg-gray-300 m-2 p-2"}>
+        <View className={"flex flex-col rounded-lg mx-1 w-full "}>
+            <View className={"mx-4 flex-row items-center border-2 gap-4 border-gray-400 rounded-lg bg-gray-300 m-1 p-1"}>
 
 
                 <Pressable
-                    className={`flex-row rounded-md px-1 my-1`}
+                    className={`flex-col w-3/12 rounded-md border-2 px-0 my-0 bg-yellow-500`}
                     onPress={() => link}
                 >
-                    <Text>{name}</Text>
-                    <Text>{" - "}</Text>
-                    <Text>{atual} / {ideal}</Text>
+                    <Text>{name} {"-"} {atual} / {ideal}</Text>
                 </Pressable>
+
                 <View
-                    className={""}
+                    className={"flex-col flex-1 text-center h-1   bg-red-700"}
                 >
-                    
+                    {/* <View className={"h-[1px] mt-2 mb-4 w-5/6 bg-gray-200"} /> */}
+                    <View
+                        className={(`h-[4px] w-${fraction ? fraction : "1/5"} bg-green-400`)}
+                    // style={ "width: 50% ;" }
+                    />
+                    {/* <View className={"flex-col items-end mx-4 border-2 border-black-500  bg-red-700 w-5 h-5"}></View> */}
+
                 </View>
+
+
+                <Pressable
+                    className={`flex-col w-1/12 rounded-md border-2 items-center bg-yellow-500`}
+                    onPress={() => link}
+                >
+                    <Text>{Math.round(qtdPercent*100)}{"%"}</Text>
+                </Pressable>
 
                 {/* { props.children } */}
 
@@ -32,6 +75,11 @@ export default function ProgressLine({ link, name, atual, ideal }: { link: strin
                 >
                     <Ionicons name="arrow-redo-outline"></Ionicons>
                 </View> */}
+                {/* <ProgressBarAndroidComponent
+                    progress={0.9} 
+                     color={Colors?.red800}
+                     style={"align: center"}
+                 />*/}
             </View>
         </View>
     );
