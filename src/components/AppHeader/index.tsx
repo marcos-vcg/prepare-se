5d23235bcdeb7859ed9972fa35e40eda9b4d255c
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2024-03-30 13:28:03 -0300

    appheader get navigation

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -7,27 +7,32 @@ import { useNavigation } from "@react-navigation/native";
 
 
 
-export default function AppHeader({ appName, ...props }: { appName: string, props?: any }) {
-   const navigation = useNavigation()
+export default function AppHeader({ appName, navigation, ...props }: { appName: string, navigation: any, props?: any }) {
+//    const navigation = useNavigation()
    
     return (
-        <View className={"flex-row space-x-10 justify-between border-2 border-gray-500 rounded-lg bg-gray-300 mt-10 mx-4"}>
+        <View className={"flex-row space-x-10 justify-between border-2 border-gray-500 rounded-lg bg-gray-300 my-3 mx-3"}>
             <View className={"flex-col justify-center m-2"}>
                 {/* <View className={"flex-col items-start justify-center m-2 bg-green-300"}></View> */}
                 <Text
-                    className={"flex-row text-lg font-bold  leading-6 text-center"}>
+                    className={"flex-row text-xl font-bold leading-6 text-center"}>
                     {/* {props?.children} */}
                     {appName}
                 </Text>
             </View>
             
             {/* <View className={"flex-col items-end m-2 border-black-500 rounded-full bg-red-700 w-10 h-10"}> */}
-            <View className={"flex-col items-end m-2"}>
-                <Ionicons name="person-circle" size={30} style={{}} />
-                <Button
-                    onPress={() => navigation.navigate('Modal')}
-                    title="Open Modal"
+            <View className={"flex-col items-end m-2"} >
+                <Ionicons
+                    name="person-outline"
+                    size={30}
+                    style={{}}
+                    onPress={() => navigation.navigate('Profile')}
                 />
+                {/* <Button
+                    onPress={() => navigation.navigate('Profile')}
+                    title="Open Modal"
+                /> */}
                 {/* <FontAwesome size={28} style={{}} /> */}
                 {/* <Image
                     style={{ width: 30, height: 30, }}
