93359de81bdb12f940262478c638b7bf37d6da79
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2024-04-01 15:36:45 -0300

    update app default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,22 @@ module.exports = {
           pressed: '#BF3C00',
           hover: '#CE4100',
         },
+        // white: {
+        //   bone: '#FBF6F1',
+        //   offWhite: '#FCF6F0',
+        //   DEFAULT: '#FFFFFF',
+        //   transparent: {
+        //     DEFAULT: '#FFFFFF4D',
+        //   },
+        // },
+        background: {
+          DEFAULT: "#3c3c3c",
+          contrast: '#515151',
+          pressed: '#666666',
+          hover: '#8c8c8c',
+          light: '#b5b5b5'
+        },
+        background2: "#3c3c3c", 
     } },
   },
   plugins: [],
